b198f87f55aa120c701ce1a840d10cf33052db2e
MHAbirKhan01753523481 <71643559+MHAbirKhan01753523481@users.noreply.github.com>
2020-09-21 19:08:51 +0600

    Add files via upload

---
+++ SelectListItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pronali.Data.ViewModel
+{
+    public class SelectListItem
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+        public bool Selected { get; set; }
+    }
+}
